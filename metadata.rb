ea0fffe845faf937cecfdb6dcda7d170d0b0bf96
RSManagedServices <sysadmin@rightscale.com>
2014-07-30 20:22:24 +0800

    Added Jenkins cookbook to the metadata

--- metadata.rb
+++ metadata.rb
@@ -32,3 +32,4 @@ recipe "datadog::dogstatsd-ruby", "Installs the Ruby dogstatsd package for custo
 # integration-specific
 recipe "datadog::cassandra", "Installs and configures the Cassandra integration"
 recipe "datadog::couchdb", "Installs and configures the CouchDB integration"
+recipe "datadog::jenkins", "Installs and configures the Jenkins integration"
